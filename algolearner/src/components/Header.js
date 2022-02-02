775dfa03436ba0396c87bec95671db702f8b52eb
Nipuna <npeiris2@uwo.ca>
2022-02-02 15:38:07 -0500

    added toolbar header

--- algolearner/src/components/Header.js
+++ algolearner/src/components/Header.js
@@ -1,43 +1,86 @@
 import Box from '@mui/material/Box';
-import { Stack } from '@mui/material';
+import { Stack, AppBar, Toolbar, Typography, MenuItem,Select } from '@mui/material';
 import { fontSize } from '@mui/system';
-
+import './Header.css'
 
 
 function Header(props){
     return(
-        <>
-        <Box
-            sx = {{
-                height: '8vh',
-                width : '100vw',
-                backgroundColor :'white',
-                borderBottom: '2px, solid black'
-            }}>
+        
+        <AppBar position='static'>
+            <Toolbar className='toolbar'>
+
+                <Box display="block">
+                    <Typography variant="h4">
+                        Sorting Visualizer
+                    </Typography>
+                    
+                </Box>
+
+                <Box display='flex' alignItems={'center'}>
+                        <Typography variant="h6" marginRight={2}>
+                            {props.level}
+                        </Typography>
+
+                        <Typography variant="h6" marginRight={2} >
+                            Choose your algorithmn
+                        </Typography>
+
+
+                        <Select defaultValue={1}>
+                            <MenuItem value={1}>MergeSort</MenuItem>
+                        </Select>
+                </Box>
+
+                
+                
+                {
+                    /*
+
+                    <Box sx = {{
+                            width: '45vw',
+                            fontSize: '2em'
+                            
+                            
+                        }}>
+                        Sorting Visualizer
+                </Box>
             
-            <Stack direction = "row" spacing = {2} alignItems={'center'} justifyContent={'center'} width = '100vw' height = '8vh' >
-                <Box sx = {{
-                        width: '45vw',
-                        fontSize: '2em'
-                        
-                        
-                    }}>
-                    Sorting Visualizer
-                </Box>
-                <Box sx = {{
-                        width: '45vw',
-                        fontSize: '2em',
-                        textAlign: 'right'
-                    }}>
-                    {props.level}
-                </Box>
+            
+            {/*<Box
+                sx = {{
+                    height: '8vh',
+                    width : '100vw',
+                    backgroundColor :'white',
+                    borderBottom: '2px, solid black'
+                }}>
+                
+                <Stack direction = "row" spacing = {2} alignItems={'center'} justifyContent={'center'} width = '100vw' height = '8vh' >
+                    <Box sx = {{
+                            width: '45vw',
+                            fontSize: '2em'
+                            
+                            
+                        }}>
+                        Sorting Visualizer
+                    </Box>
+                    <Box sx = {{
+                            width: '45vw',
+                            fontSize: '2em',
+                            textAlign: 'right'
+                        }}>
+                        {props.level}
+                    </Box>
 
-               
-            </Stack>
+                
+                </Stack>
 
-        </Box>
+            </Box>*/}
 
-        </>
+            </Toolbar>
+        </AppBar>
+
+        
     );
 }
 
