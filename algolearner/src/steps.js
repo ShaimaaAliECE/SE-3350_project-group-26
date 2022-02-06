c50d7754939883c135da31af122e42c4ad71975e
Hisham632 <77436699+Hisham632@users.noreply.github.com>
2022-02-06 02:21:54 -0500

    createdTree

---
+++ algolearner/src/steps.js
@@ -0,0 +1,178 @@
+var nodes1=[
+    { id: 1, label: 'Node 1' }
+]
+console.log("here")
+//console.log(nodes1[0]['label']='4,5')
+//nodes1[0]['label']='4,5'// to change label of node
+var nodes2=[
+    { id: 1, label: 'Node 1' },
+    { id: 2, label: 'Node 2' },
+    { id: 3, label: 'Node 3' },
+]
+var nodes3=[
+    { id: 1, label: 'Node 1' },
+    { id: 2, label: 'Node 2' },
+    { id: 3, label: 'Node 3' },
+    { id: 4, label: 'Node 4' },
+    { id: 5, label: 'Node 5' },
+]
+
+var nodes4=[
+    { id: 1, label: 'Node 1' },
+    { id: 2, label: 'Node 2' },
+    { id: 3, label: 'Node 3' },
+    { id: 4, label: 'Node 4' },
+    { id: 5, label: 'Node 5' },
+    { id: 6, label: 'Node 6' },
+    { id: 7, label: 'Node 7' },
+]
+
+
+var nodes5=[
+    { id: 1, label: 'Node 1' },
+    { id: 2, label: 'Node 2' },
+    { id: 3, label: 'Node 3' },
+    { id: 4, label: 'Node 4' },
+    { id: 5, label: 'Node 5' },
+    { id: 6, label: 'Node 6' },
+    { id: 7, label: 'Node 7' },
+    { id: 8, label: 'Node 8' },
+    { id: 9, label: 'Node 9' },
+]
+
+var nodes6=[
+    { id: 1, label: 'Node 1' },
+    { id: 2, label: 'Node 2' },
+    { id: 3, label: 'Node 3' },
+    { id: 4, label: 'Node 4' },
+    { id: 5, label: 'Node 5' },
+    { id: 10, label: 'Node 10' },
+    { id: 11, label: 'Node 11' },
+]
+
+
+var nodes7=[
+    { id: 1, label: 'Node 1' },
+    { id: 2, label: 'Node 2' },
+    { id: 3, label: 'Node 3' },
+    { id: 12, label: 'Node 12' },
+    { id: 13, label: 'Node 13' },
+]
+
+var nodes8=[
+    { id: 1, label: 'Node 1' },
+    { id: 2, label: 'Node 2' },
+    { id: 3, label: 'Node 3' },
+    { id: 12, label: 'Node 12' },
+    { id: 13, label: 'Node 13' },
+    { id: 14, label: 'Node 14' },
+    { id: 15, label: 'Node 15' },
+]
+var nodes9=[
+    { id: 1, label: 'Node 1' },
+    { id: 2, label: 'Node 2' },
+    { id: 3, label: 'Node 3' },
+    { id: 12, label: 'Node 12' },
+    { id: 13, label: 'Node 13' },
+    { id: 14, label: 'Node 14' },
+    { id: 15, label: 'Node 15' },
+    { id: 16, label: 'Node 16' },
+    { id: 17, label: 'Node 17' },
+]
+
+var nodes10=[
+    { id: 1, label: 'Node 1' },
+    { id: 2, label: 'Node 2' },
+    { id: 3, label: 'Node 3' },
+    { id: 12, label: 'Node 12' },
+    { id: 13, label: 'Node 13' },
+    { id: 18, label: 'Node 18' },
+    { id: 19, label: 'Node 19' }
+    
+]
+
+var allNodes=[
+{ id: 1, label: 'Node 1' },
+{ id: 2, label: 'Node 2' },
+{ id: 3, label: 'Node 3' },
+{ id: 4, label: 'Node 4' },
+{ id: 5, label: 'Node 5' },
+{ id: 6, label: 'Node 6' },
+{ id: 7, label: 'Node 7' },
+{ id: 8, label: 'Node 8' },
+{ id: 9, label: 'Node 9' },
+{ id: 10, label: 'Node 10' },
+{ id: 11, label: 'Node 11' },
+{ id: 12, label: 'Node 12' },
+{ id: 13, label: 'Node 13' },
+{ id: 14, label: 'Node 14' },
+{ id: 15, label: 'Node 15' },
+{ id: 16, label: 'Node 16' },
+{ id: 17, label: 'Node 17' },
+{ id: 18, label: 'Node 18' },
+{ id: 19, label: 'Node 19' }
+
+]
+
+function step1()
+{
+    nodes1[0]['label']='4,7'// to change label of node
+
+    return nodes1
+}
+
+
+function step2()
+{
+    return nodes2
+}
+
+
+function step3()
+{
+    return nodes3
+}
+
+
+function step4()
+{
+    return nodes4
+}
+
+
+function step5()
+{
+    return nodes5
+}
+
+
+function step6()
+{
+    return nodes6
+}
+
+
+function step7()
+{
+    return nodes7
+}
+
+
+function step8()
+{
+    return nodes8
+}
+
+
+function step9()
+{
+    return nodes9
+}
+
+
+function step10()
+{
+    return nodes10
+}
+
+export {step1,step2,step3,step4,step5,step6,step7,step8,step9,step10};
--- algolearner/src/tree.js
+++ algolearner/src/tree.js
@@ -1,29 +1,40 @@
 import React, { useEffect, useRef } from 'react';
 import { DataSet, Network} from 'vis-network/standalone/esm/vis-network';
+import {step1,step2,step3,step4,step5,step6,step7,step8,step9,step10} from './steps';
 
 const VisNetwork = () => {
   // A reference to the div rendered by this component
-  const domNode = useRef(null);
-
+  const rootNode = useRef(null);
   // A reference to the vis network instance
   const network = useRef(null);
 
+  const stepsList=[step1(),step2(),step3(),step4(),step5(),step6(),step7(),step8(),step9(),step10()]// step list(of each change)
   // An array of nodes
-  const nodes = new DataSet([
-    { id: 1, label: 'Node 1' },
-    { id: 2, label: 'Node 2' },
-    { id: 3, label: 'Node 3' },
-    { id: 4, label: 'Node 4' },
-    { id: 5, label: 'Node 5' },
-    
-  ]);// it doesnt accept input but what we could do is that after each step we'd add our own textbox asking them to enter the next step and check if theyre right
+  const nodes = new DataSet(stepsList[0])
+  // it doesnt accept input but what we could do is that after each step we'd add our own textbox asking them to enter the next step and check if theyre right
     // if they are we display the next step in the tree otherwise tell them to try again
 
   // An array of edges
   const edges = new DataSet([
     { from: 1, to: 2 },
     { from: 1, to: 3 },
-    
+    { from: 2, to: 4 },
+    { from: 2, to: 5 },
+    { from: 4, to: 6 },
+    { from: 4, to: 7 },
+    { from: 6, to: 8 },
+    { from: 6, to: 9 },
+    { from: 5, to: 10 },
+    { from: 5, to: 11 },
+    { from: 3, to: 12 },
+    { from: 3, to: 13 },
+    { from: 12, to: 14 },
+    { from: 12, to: 15 },
+    { from: 14, to: 17 },
+    { from: 14, to: 16 },
+    { from: 13, to: 18 },
+    { from: 13, to: 19 },
+
   ]);
   
   const data = {
@@ -31,9 +42,9 @@ const VisNetwork = () => {
     edges
   };
 
-  const options = {
+  const options = 
+  {
     autoResize: true,
-
     edges: {
       smooth: {
         enabled: true,
@@ -107,18 +118,17 @@ const VisNetwork = () => {
       },
 
     },
-  };;
+  };
   
-  useEffect(
-    () => {
-      
-      network.current = new Network(domNode.current, data, options);
-    },
-    [domNode, network, data, options]
+  useEffect(() =>
+   {
+      network.current = new Network(rootNode.current, data, options);
+   },
+    [rootNode, network, data, options]
   );
 
   return (
-    <div ref = { domNode } />
+    <div ref = { rootNode } />
   );
 };
 
