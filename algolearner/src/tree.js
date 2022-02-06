3bf37f582846df816da17e0fb591a72852db6f72
Hisham632 <77436699+Hisham632@users.noreply.github.com>
2022-02-05 21:36:45 -0500

    myArt

--- algolearner/src/tree.js
+++ algolearner/src/tree.js
@@ -30,8 +30,76 @@ const VisNetwork = () => {
     edges
   };
 
-  const options = {};
+  const options = {
+    autoResize: true,
 
+    edges: {
+      smooth: {
+        enabled: true,
+        type: "dynamic",
+        roundness: 0.5
+      },
+      color: {
+        color:'#a80000',
+        highlight:'#a80000',
+        hover: '#848484',
+        inherit: 'from',
+        opacity:1.0
+      },
+      dashes: false,
+      font:
+      {
+        color: '#4bde31',
+
+      },
+
+    },
+    layout: {
+      hierarchical: {
+      },
+    },
+    physics: false,
+
+    interaction:{
+      dragNodes:false,
+      dragView: false,
+      hideEdgesOnDrag: false,
+      hideEdgesOnZoom: false,
+      hideNodesOnDrag: false,
+      hover: false,
+      hoverConnectedEdges: false,
+      keyboard: {
+        enabled: false,
+        speed: {x: 10, y: 10, zoom: 0.02},
+        bindToWindow: false,
+        autoFocus: false,
+      },
+      multiselect: false,
+      navigationButtons: false,
+      selectable: false,
+      selectConnectedEdges: false,
+      tooltipDelay: 300,
+      zoomSpeed: 1,
+      zoomView: true
+    },
+    nodes:{
+      color: {
+        border: '#4309b0',
+        background: '#395596',
+        highlight: {
+          border: '#4309b0',
+          background: '#4309b0'
+        }
+      },
+      font:{
+      color: '#ed6b6b',
+      size: 20, // px
+      face: 'arial',
+      },
+
+    },
+  };;
+  
   useEffect(
     () => {
       network.current = new Network(domNode.current, data, options);
