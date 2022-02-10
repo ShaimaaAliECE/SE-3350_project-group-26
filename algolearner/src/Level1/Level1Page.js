93e5088433a291903e02e391a0e4356ef3845f5d
axrios <johnson.yong.135x@gmail.com>
2022-02-10 16:18:26 -0500

    Fixing UI

--- algolearner/src/Level1/Level1Page.js
+++ algolearner/src/Level1/Level1Page.js
@@ -1,7 +1,7 @@
 import Box from '@mui/material/Box';
 import {Stack,Item,TextField,Button, Typography} from '@mui/material';
 import Header from '../components/Header';
-import {useState, UseState} from 'react';
+import {useEffect, useState, UseState} from 'react';
 import VisNetwork from '../tree.js'
 
 
@@ -14,6 +14,25 @@ function stringToArr(stringVal){ //This converts a string into an array
     return arr;
 
 }
+
+
+/*This generates the random array int */
+function generateArray(){
+
+    let arr = [];
+    var num;
+    for(let i =0; i< 10; i++){
+        num = Math.floor(Math.random() * 20) + 1;
+        arr.push(num);
+        
+    }
+
+    return arr;
+
+}
+
+
+
 const steps=[0,1,2,3,4,3,2,5,2,1,6,7,8,7,6,9,6,1,0]
 
 function LevelOne(props){
@@ -22,33 +41,37 @@ function LevelOne(props){
     const [testing11, setTesting] = useState([]);
     const [count, setCount] = useState(0);
     const [fullArr, setFull] = useState([]);
+    useEffect(()=>{
+        setNumArr(generateArray());
+    },[]);
+
+    
 
 
     const check = () =>{
         var send = {
-            "depth": count,
-            "arr": numArr
-        }
-        fetch('http://localhost:3001/api/getStep', {  //connect to backend
-        method: 'POST', //post
-        credentials: 'include', 
-        headers: {
-            'Content-Type': 'application/json',
-    },
+                "depth": count,
+                "arr": numArr
+            }
+            fetch('http://localhost:3001/api/getStep', {  //connect to backend
+            method: 'POST', //post
+            credentials: 'include', 
+            headers: {
+                'Content-Type': 'application/json',
+        },
         body: JSON.stringify(send), //body is the set data from earlier
-    })
-        .then(response => (response.json()))
-
-        .then(data => {
-            setTesting(data.break);
-            setFull(data.full);     
-            console.log(fullArr);
-
         })
-        .catch((error) => {
-            console.error('Error:', error);
-    });
+            .then(response => (response.json()))
 
+            .then(data => {
+                setTesting(data.break);
+                setFull(data.full);     
+                console.log(fullArr);
+
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+        });
     }
     return(
         <>
@@ -59,8 +82,63 @@ function LevelOne(props){
                 width: '100vw',
                 backgroundColor: 'white'
             }}>
+                <Stack direction = "column" gap = {3} width = '100vw' alignItems={'center'} justifyItems={'center'} >
+                    <Stack direction = "row" gap = {3} marginTop = {'2vh'}>
+                        {numArr.map((v) =>{
+                            return(
+                                <Box sx= {{
+                                    width: '1vw'
 
-                <Stack width = '100vw' alignItems={'center'} justifyItems={'center'}>
+                                }}>
+                                    {v}
+                                </Box>
+                            );
+                        })}
+                        
+                    </Stack>
+
+                    <Stack direction={'row'}
+                    >
+                        <Button
+                            onClick = {()=>{
+
+                                setCount(count -1);
+                            }}>
+                            Prev
+                        </Button>
+
+                        <Button
+                            onClick = {()=>{
+                                
+                                setCount(count +1);
+                            //   console.log(count);
+                            }}>
+                            
+                            Next
+                        </Button>
+
+
+                    </Stack>
+                    <Stack>
+                        {count}
+                    </Stack>
+                    
+                </Stack>
+
+                    {console.log(count)}
+                <VisNetwork treeForm={steps[count]} count={count}/>{/* We need to make it so after count 19 it the buttons dont work */}
+            
+            </Box>
+
+        </>
+
+    );
+
+}
+
+export default LevelOne;
+
+ {/*<Stack width = '100vw' alignItems={'center'} justifyItems={'center'}>
                     <Box 
                     sx= {{
                         marginTop: '5vw',
@@ -78,8 +156,9 @@ function LevelOne(props){
                                 console.log(numberString);
                             }}/>
                     </Box>
-                </Stack>
+                </Stack>*/}
 
+{/*
                 <Button
                 onClick = {()=>{
                     setNumArr(stringToArr(numberString));
@@ -93,82 +172,7 @@ function LevelOne(props){
                         check();
                     }}>
                     TESTING
-                </Button>
-
-
-
-                <Stack direction={'row'}
-                marginLeft = '2vw'>
-                    <Button
-                        onClick = {()=>{
-
-                            setCount(count -1);
-                        }}>
-                        Prev
-                    </Button>
-
-                    <Button
-                        onClick = {()=>{
-                            
-                            setCount(count +1);
-                         //   console.log(count);
-                        }}>
-                        
-                        Next
-                    </Button>
-
-
-                </Stack>
-
-                    {console.log(count)}
-                <VisNetwork treeForm={steps[count]} count={count}/>{/* We need to make it so after count 19 it the buttons dont work */}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-               {/* <Stack direction={'row'}
-                marginLeft = '2vw'>
-                    {numArr.map((v) =>{
-                        return(
-                            <Box 
-                            sx = {{
-                                border: '2px solid Black',
-                                width: "20px"
-                            }}>
-                                {v}
-                            </Box>
-                           
-                        );
-                    })}
-                </Stack>
-*/}
-
-                
-                {/* What ever is below here is for testing purposes */}
-                
-
-            
-            
-            </Box>
-
-        </>
-
-    );
-
-}
-
-export default LevelOne;
+                </Button>*/}
 
 
 
