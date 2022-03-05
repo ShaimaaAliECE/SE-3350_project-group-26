6aeedee66e906aea027fdfa95213e1c9c0cc60a5
Nipuna <npeiris2@uwo.ca>
2022-03-04 21:08:58 -0500

    changed ip final

--- algolearner/src/components/api/index.js
+++ algolearner/src/components/api/index.js
@@ -7,7 +7,7 @@ export const getStep = async (depth,arr) => {
             arr: arr
         }
 
-        //use for localhost testing http://localhost:80/api/getStep
+        //use for localhost testing http://localhost:3001/api/getStep
         const response = await fetch('http://35.225.122.85:3001/api/getStep', {method : 'POST', headers: { 'Content-Type': 'application/json' 
           }, body: JSON.stringify(body_data) })
            
